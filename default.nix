98738ff24eec455358459d6bf7581dfe0e324278
Linus Heckemann <git@sphalerite.org>
2026-09-23 20:46:52 +0200

    Add bigTree

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ system ? builtins.currentSystem, seed ? "" }:
+{ system ? builtins.currentSystem, seed ? "", bigTreeWidths ? [ 5 5 5 5 ] }:
 rec {
   # pkgsStatic.busybox for the respective systems, from
   # nixpkgs b36e8f733df3ca8a60fec114e1ce85e15fb198b2
@@ -36,4 +36,24 @@ rec {
       echo ${dependent} > $lib
     '';
   };
+
+  
+  bigTree = let
+    treeDrv = n: deps: mkDerivation {
+      name = "tree-${toString n}";
+      outputs = ["out"];
+      inherit deps seed;
+      script = ''
+        echo $deps > $out
+      '';
+    };
+
+    makeTreeDrvs = depth: widths: if widths == [] then [(treeDrv depth [])] else
+      let
+        width = builtins.head widths;
+        range = n: if n <= 0 then [] else (range (n - 1)) ++ [n];
+        deps = builtins.map (n: makeTreeDrvs (depth*10+n) (builtins.tail widths)) (range width);
+      in treeDrv depth deps;
+  in 
+    makeTreeDrvs 0 bigTreeWidths;
 }
